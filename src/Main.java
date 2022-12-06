d8bac292045cbb09d6ece73c5b9937625c8e8a48
tatiana <tatiana.tokmantseva@gmail.com>
2022-12-06 21:35:40 +0500

    Решение практической части билета №5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String [] Books = {"Пушкин А.С. - Руслан и Люмила", "Тургенев И.С. - Отцы и дети"} ;
+        String arrayBooks = String.join(", ", Books );
+        System.out.println(arrayBooks);
     }
 }
